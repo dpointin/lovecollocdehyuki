ebf68e5af9f3697a78de2f653b085cd2dc05fcf0
Matthias64 <matthias.larre@gmail.com>
2017-02-23 19:14:01 +0100

    BAM

--- FinalRound/input_reader.py
+++ FinalRound/input_reader.py
@@ -1,17 +1,12 @@
 from def_class import *
 
-MAX_CAPACITY = 0
-cache_servers = []
-endpoints = []
-requests = []
-videos = []
 
 def read_input(filename):
-    global MAX_CAPACITY,cache_servers,requests,videos,endpoints
     with open(filename, 'r') as f:
         n_videos, n_endpoints, n_requests, n_cache_servers, MAX_CAPACITY = map(int, f.readline().strip().split())
         videos = [Video(i, int(f.readline())) for i in xrange(n_videos)]
         cache_servers = [CacheServeur(i) for i in range(n_cache_servers)]
+        endpoints = []
         for i in xrange(n_endpoints):
             e = EndPoint(i)
             e.lat_Server, n_cache_servers_by_endpoint = map(int, f.readline().strip().split())
@@ -19,6 +14,8 @@ def read_input(filename):
                 cache_server_index = int(f.readline())
                 latency = int(f.readline())
                 e.cacheServers[cache_servers[cache_server_index]] = latency
+        requests = []
         for i in xrange(n_requests):
             i_video, i_endpoint, n = map(int, f.readline().strip().split())
             requests.append(Request(i, videos[i_video], endpoints[i_endpoint], n))
+    return Probleme(MAX_CAPACITY, cache_servers, requests, videos, endpoints)
--- FinalRound/main.py
+++ FinalRound/main.py
@@ -3,6 +3,7 @@ from input_reader import read_input
 if __name__ == '__main__':
     infile = 'me_at_the_zoo.in'
     outfile = infile.split('.')[0] + '.out'
-    read_input(infile)
+    problem = read_input(infile)
+
     output_file = open(outfile, 'w+')
     output_file.write("ok")

63d06be459f6d36055ebaf67f9928c2542fc4bf1
dpointin <damien.pointin>
2017-02-23 19:15:01 +0100

    str + pb

--- FinalRound/def_class.py
+++ FinalRound/def_class.py
@@ -14,6 +14,10 @@ class CacheServeur:
         self.videos = []
         self.id = id
 
+        def __str__(self):
+            chaine = "CacheServeur {}".format(id)
+            return chaine
+
 
 class EndPoint:
     def __init__(self, id, lat_dataServer, dico):
@@ -21,10 +25,26 @@ class EndPoint:
         self.cacheServeur = dico
         self.id = id
 
+    def __str__(self):
+        chaine = "End point {}".format(id)
+        return chaine
+
 
 class Request:
-    def __init__(self, id, Video, endPoint, nombre):
+    def __init__(self, id, video, endPoint, nombre):
         self.nb = nombre
         self.endPoint = endPoint
         self.id = id
         self.video = video
+
+    def __str__(self):
+        chaine = "Request {}".format(id)
+        return chaine
+
+class Probleme:
+    def __init__(self, max_cap, cache_serveur, resquest, video, endpoints):
+        self.max_cap=max_cap
+        self.cache_serveur=cache_serveur
+        self.resquest=resquest
+        self.video=video
+        self.endpoints=endpoints
